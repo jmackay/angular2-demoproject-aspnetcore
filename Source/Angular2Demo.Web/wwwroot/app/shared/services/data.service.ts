929c280af85b3079307e661c207410f0f310adea
John Mackay <jmackay84@gmail.com>
2016-03-23 11:53:54 -0700

    adding ability to call static json files, show assigned users on card

--- Source/Angular2Demo.Web/wwwroot/app/shared/services/data.service.ts
+++ Source/Angular2Demo.Web/wwwroot/app/shared/services/data.service.ts
@@ -12,15 +12,21 @@ export class DataService {
     constructor() {
         this._store = new JSData.DS();
         var adapter: any = new DSHttpAdapter();
-        adapter.defaults.basePath = "/api";
-        this._store.registerAdapter("http", adapter, { default: true });
-		this.defineJsDataRelationships();
 
-		//this.reloadAll();
+		var useStatic = false; // If you don't want to use a backend API you can turn this on to load from static json files, saving and updates will error on their post/put requests!
+
+		if (useStatic) {
+			adapter.defaults.basePath = "/api/static";
+			this._store.registerAdapter("http", adapter, { default: true });
+			this.defineJsDataRelationshipsStatic();
+		} else {
+			adapter.defaults.basePath = "/api";
+			this._store.registerAdapter("http", adapter, { default: true });
+			this.defineJsDataRelationships();
+		}
     }
-
+	
 	reloadAll() {
-		// Preload all data
 		this._store.clear();
 
 		this.Users.findAll().then(data => {
@@ -123,6 +129,108 @@ export class DataService {
 		this.Categories = this._store.defineResource<server.Category>({
 			name: "category",
 			endpoint: "categories",
+			relations: {
+				hasMany: {
+					card: {
+						localField: "cards",
+						foreignKey: "categoryId"
+					}
+				}
+			}
+		});
+    }
+
+	defineJsDataRelationshipsStatic() {
+        this.Users = this._store.defineResource<server.User>({
+            name: "user",
+            endpoint: "users.json",
+			computed: {
+				assignedCards: {
+					get: function (): server.Card[] {
+						return (<server.User>this).assignedTo.map(x => x.card)
+					}
+				}
+			},
+            relations: {
+                hasMany: {
+                    card: {
+                        localField: "createdCards",
+                        foreignKey: "createdById"
+                    },
+                    cardAssignment: [{
+                        localField: "assignedTo",
+                        foreignKey: "assignedToId"
+                    }, {
+                            localField: "createdAssignments",
+                            foreignKey: "createdById"
+                        }]
+                }
+            }
+        });
+
+        this.CardAssignments = this._store.defineResource<server.CardAssignment>({
+			name: "cardAssignment",
+			endpoint: "cardAssignments.json",
+			relations: {
+				belongsTo: {
+					card: {
+						localField: "card",
+						localKey: "cardId"
+					},
+					user: [{
+						localField: "assignedTo",
+						localKey: "assignedToId"
+					}, {
+							localField: "createdBy",
+							localKey: "createdById"
+						}]
+				}
+			}
+		});
+
+		this.Cards = this._store.defineResource<server.Card>({
+			name: "card",
+			endpoint: "cards.json",
+			relations: {
+				belongsTo: {
+					user: {
+						localField: "createdBy",
+						localKey: "createdById"
+					},
+					stack: {
+						localField: "stack",
+						localKey: "stackId"
+					},
+					category: {
+						localField: "category",
+						localKey: "categoryId"
+					}
+				},
+				hasMany: {
+					cardAssignment: {
+						localField: "cardAssignments",
+						foreignKey: "cardId"
+					}
+				}
+			}
+		});
+
+		this.Stacks = this._store.defineResource<server.Stack>({
+			name: "stack",
+			endpoint: "stacks.json",
+			relations: {
+				hasMany: {
+					card: {
+						localField: "cards",
+						foreignKey: "stackId"
+					}
+				}
+			}
+		});
+
+		this.Categories = this._store.defineResource<server.Category>({
+			name: "category",
+			endpoint: "categories.json",
 			relations: {
 				hasMany: {
 					card: {
